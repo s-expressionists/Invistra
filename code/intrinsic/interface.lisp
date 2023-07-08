f51469f8f6880572009bc68d311166e0083c3f99
Tarn W. Burton <twburton@gmail.com>
2023-07-08 09:04:46 -0400

    Fix formatter interface

--- code/intrinsic/interface.lisp
+++ code/intrinsic/interface.lisp
@@ -5,7 +5,7 @@
 (defun format (destination control-string &rest args)
   (apply #'invistra:format *client* destination control-string args))
 
-(defun formatter (control-string)
+(defmacro formatter (control-string)
   (invistra:formatter '*client* control-string))
 
 (define-compiler-macro format (&whole form destination control-string &rest args)
